c2f8b1d6f3b40d4d356558c5d874691e145b833f
LA-admin <120379826+lu-shuaipeng@users.noreply.github.com>
2024-03-12 10:56:10 +0800

    pull commit

--- src/main/java/com/lsp/GitTest.java
+++ src/main/java/com/lsp/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master 冲突合并test");
         System.out.println("hot-fix 合并冲突test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
